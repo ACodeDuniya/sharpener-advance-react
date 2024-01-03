58df9d6c395b3fbac0a12a859d8aa530fcb39f9c
ACodeDuniya <therockapes1234@gmail.com>
2024-01-03 12:23:26 +0530

    Using multiple states together

--- basic_file/src/components/NewExpense/ExpenseForm.js
+++ basic_file/src/components/NewExpense/ExpenseForm.js
@@ -1,36 +1,3 @@
-// import React from "react";
-// import "./ExpenseForm.css";
-// const ExpenseForm = () => {
-//     const titleChangeHandler = () => {
-//         console.log("You simply have to use vanilla JS. - onChange and event object");
-//     };
-//     return (
-//         <form>
-//             <div className="new-expense__controls">
-//                 <div className="new-expense__control">
-//                     <label>Title</label>
-//                     <input type="text" onChange={titleChangeHandler} />
-//                 </div>
-//                 <div className="new-expense__control">
-//                     <label>Amount</label>
-//                     <input type="number" onChange={titleChangeHandler}/>
-//                 </div>
-//                 <div className="new-expense__control">
-//                     <label>Title</label>
-//                     <input type="date" onChange={titleChangeHandler}/>
-//                 </div>
-//             </div>
-//             <div className="new-expense__actions">
-//                 <button type="submit">Add Expense</button>
-//             </div>
-//         </form>
-//     );
-// };
-
-// export default ExpenseForm;
-
-
-// adding-multiple states
 import React from "react";
 import { useState } from "react";
 import "./ExpenseForm.css";
